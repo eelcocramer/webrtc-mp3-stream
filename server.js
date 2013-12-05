7fe18ad273c1f9543cba9cf1f2c013470301347a
eelcocramer <eelco@hailendal.org>
2013-12-05 09:57:53 +0100

    Adds new line

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ var static = require('node-static');
 //
 // Create a node-static server instance to serve the './public' folder
 //
+
 var file = new(static.Server)('./client');
 
 var server = require('http').createServer(function (request, response) {
