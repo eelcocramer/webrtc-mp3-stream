1df651a760088b8fa8b739b8320641873d7a0d5c
Eelco Cramer <eelco@servicelab.org>
2021-06-19 16:50:14 +0200

    Fixes api's for firefox

--- client/js/adapter.js
+++ client/js/adapter.js
@@ -6,127 +6,124 @@ var webrtcDetectedBrowser = null;
 var webrtcDetectedVersion = null;
 
 function trace(text) {
-  // This function is used for logging.
-  if (text[text.length - 1] == '\n') {
-    text = text.substring(0, text.length - 1);
-  }
-  console.log((performance.now() / 1000).toFixed(3) + ": " + text);
+    // This function is used for logging.
+    if (text[text.length - 1] == '\n') {
+        text = text.substring(0, text.length - 1);
+    }
+    console.log((performance.now() / 1000).toFixed(3) + ": " + text);
 }
 
-if (navigator.mozGetUserMedia) {
-  console.log("This appears to be Firefox");
+if (navigator.mediaDevices.getUserMedia) {
+    console.log("This appears to be Firefox");
 
-  webrtcDetectedBrowser = "firefox";
+    webrtcDetectedBrowser = "firefox";
 
-  // The RTCPeerConnection object.
-  RTCPeerConnection = mozRTCPeerConnection;
+    // Get UserMedia (only difference is the prefix).
+    // Code from Adam Barth.
+    getUserMedia = navigator.mediaDevices.getUserMedia.bind(navigator);
 
-  // The RTCSessionDescription object.
-  RTCSessionDescription = mozRTCSessionDescription;
+    // Creates Turn Uri with new turn format.
+    createIceServer = function(turn_url, username, password) {
+        var iceServer = {
+            'url': turn_url,
+            'credential': password,
+            'username': username
+        };
+        return iceServer;
+    };
 
-  // The RTCIceCandidate object.
-  RTCIceCandidate = mozRTCIceCandidate;
+    // Attach a media stream to an element.
+    attachMediaStream = function(element, stream) {
+        console.log("Attaching media stream");
+        element.mozSrcObject = stream;
+        element.play();
+    };
 
-  // Get UserMedia (only difference is the prefix).
-  // Code from Adam Barth.
-  getUserMedia = navigator.mozGetUserMedia.bind(navigator);
+    reattachMediaStream = function(to, from) {
+        console.log("Reattaching media stream");
+        to.mozSrcObject = from.mozSrcObject;
+        to.play();
+    };
 
-  // Creates Turn Uri with new turn format.
-  createIceServer = function(turn_url, username, password) {
-    var iceServer = { 'url': turn_url,
-                      'credential': password,
-                      'username': username };
-    return iceServer;
-  };
+    // Fake get{Video,Audio}Tracks
+    MediaStream.prototype.getVideoTracks = function() {
+        return [];
+    };
 
-  // Attach a media stream to an element.
-  attachMediaStream = function(element, stream) {
-    console.log("Attaching media stream");
-    element.mozSrcObject = stream;
-    element.play();
-  };
-
-  reattachMediaStream = function(to, from) {
-    console.log("Reattaching media stream");
-    to.mozSrcObject = from.mozSrcObject;
-    to.play();
-  };
-
-  // Fake get{Video,Audio}Tracks
-  MediaStream.prototype.getVideoTracks = function() {
-    return [];
-  };
-
-  MediaStream.prototype.getAudioTracks = function() {
-    return [];
-  };
+    MediaStream.prototype.getAudioTracks = function() {
+        return [];
+    };
 } else if (navigator.webkitGetUserMedia) {
-  console.log("This appears to be Chrome");
+    console.log("This appears to be Chrome");
 
-  webrtcDetectedBrowser = "chrome";
-  webrtcDetectedVersion =
-             parseInt(navigator.userAgent.match(/Chrom(e|ium)\/([0-9]+)\./)[2]);
+    webrtcDetectedBrowser = "chrome";
+    webrtcDetectedVersion =
+        parseInt(navigator.userAgent.match(/Chrom(e|ium)\/([0-9]+)\./)[2]);
 
-  // For pre-M28 chrome versions use old turn format, else use the new format.
-  if (webrtcDetectedVersion < 28) {
-    createIceServer = function(turn_url, username, password) {
-      var iceServer = { 'url': 'turn:' + username + '@' + turn_url,
-                        'credential': password };
-      return iceServer;
-    };
-  } else {
-    createIceServer = function(turn_url, username, password) {
-      var iceServer = { 'url': turn_url,
-                        'credential': password,
-                        'username': username };
-      return iceServer;
-    };
-  }
-
-  // The RTCPeerConnection object.
-  RTCPeerConnection = webkitRTCPeerConnection;
-
-  // Get UserMedia (only difference is the prefix).
-  // Code from Adam Barth.
-  getUserMedia = navigator.webkitGetUserMedia.bind(navigator);
-
-  // Attach a media stream to an element.
-  attachMediaStream = function(element, stream) {
-    if (typeof element.srcObject !== 'undefined') {
-      element.srcObject = stream;
-    } else if (typeof element.mozSrcObject !== 'undefined') {
-      element.mozSrcObject = stream;
-    } else if (typeof element.src !== 'undefined') {
-      element.src = URL.createObjectURL(stream);
+    // For pre-M28 chrome versions use old turn format, else use the new format.
+    if (webrtcDetectedVersion < 28) {
+        createIceServer = function(turn_url, username, password) {
+            var iceServer = {
+                'url': 'turn:' + username + '@' + turn_url,
+                'credential': password
+            };
+            return iceServer;
+        };
     } else {
-      console.log('Error attaching stream to element.');
+        createIceServer = function(turn_url, username, password) {
+            var iceServer = {
+                'url': turn_url,
+                'credential': password,
+                'username': username
+            };
+            return iceServer;
+        };
     }
-  };
 
-  reattachMediaStream = function(to, from) {
-    to.src = from.src;
-  };
+    // The RTCPeerConnection object.
+    RTCPeerConnection = webkitRTCPeerConnection;
 
-  // The representation of tracks in a stream is changed in M26.
-  // Unify them for earlier Chrome versions in the coexisting period.
-  if (!webkitMediaStream.prototype.getVideoTracks) {
-    webkitMediaStream.prototype.getVideoTracks = function() {
-      return this.videoTracks;
-    };
-    webkitMediaStream.prototype.getAudioTracks = function() {
-      return this.audioTracks;
-    };
-  }
+    // Get UserMedia (only difference is the prefix).
+    // Code from Adam Barth.
+    getUserMedia = navigator.webkitGetUserMedia.bind(navigator);
 
-  // New syntax of getXXXStreams method in M26.
-  if (!webkitRTCPeerConnection.prototype.getLocalStreams) {
-    webkitRTCPeerConnection.prototype.getLocalStreams = function() {
-      return this.localStreams;
+    // Attach a media stream to an element.
+    attachMediaStream = function(element, stream) {
+        if (typeof element.srcObject !== 'undefined') {
+            element.srcObject = stream;
+        } else if (typeof element.mozSrcObject !== 'undefined') {
+            element.mozSrcObject = stream;
+        } else if (typeof element.src !== 'undefined') {
+            element.src = URL.createObjectURL(stream);
+        } else {
+            console.log('Error attaching stream to element.');
+        }
     };
-    webkitRTCPeerConnection.prototype.getRemoteStreams = function() {
-      return this.remoteStreams;
+
+    reattachMediaStream = function(to, from) {
+        to.src = from.src;
     };
-  }
+
+    // The representation of tracks in a stream is changed in M26.
+    // Unify them for earlier Chrome versions in the coexisting period.
+    if (!webkitMediaStream.prototype.getVideoTracks) {
+        webkitMediaStream.prototype.getVideoTracks = function() {
+            return this.videoTracks;
+        };
+        webkitMediaStream.prototype.getAudioTracks = function() {
+            return this.audioTracks;
+        };
+    }
+
+    // New syntax of getXXXStreams method in M26.
+    if (!webkitRTCPeerConnection.prototype.getLocalStreams) {
+        webkitRTCPeerConnection.prototype.getLocalStreams = function() {
+            return this.localStreams;
+        };
+        webkitRTCPeerConnection.prototype.getRemoteStreams = function() {
+            return this.remoteStreams;
+        };
+    }
 } else {
-  console.log("Browser does not appear to be WebRTC-capable");
+    console.log("Browser does not appear to be WebRTC-capable");
 }
